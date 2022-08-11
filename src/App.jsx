6f3618270e2d8d07cd50d80cc9613310ee31faf7
Joel Trinidad <jth_2929@hotmail.com>
2022-08-10 20:15:51 -0500

    feat: images and routes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
+import "./App.css";
 import "semantic-ui-css/semantic.min.css";
-import { Button, Icon } from "semantic-ui-react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
+import Series from "./pages/Series";
+import Comics from "./pages/Comics";
+import Error404 from "./pages/Error404";
 
 function App() {
   return (
     <div>
-      Creando nuestra app de marvel
-      <Button animated>
-        <Button.Content visible>Next</Button.Content>
-        <Button.Content hidden>
-          <Icon name="arrow right" />
-        </Button.Content>
-      </Button>
+      <Router>
+        <Switch>
+          <Route path="/" exact component={() => <Home />} />
+          <Route path="/inicio" exact component={() => <Home />} />
+          <Route path="/series" exact component={() => <Series />} />
+          <Route path="/comics" exact component={() => <Comics />} />
+          <Route component={() => <Error404 />} />
+        </Switch>
+      </Router>
     </div>
   );
 }
