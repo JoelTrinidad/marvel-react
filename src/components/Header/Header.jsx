ad80a76c8583d05d857076278cf3505c92766ea5
Joel Trinidad <jth_2929@hotmail.com>
2022-08-13 17:38:56 -0500

    feat: menu functionality

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,24 @@
 import { useState } from "react";
+import { useHistory, useLocation } from "react-router-dom";
 import { Menu } from "semantic-ui-react";
 import "./Header.scss";
 
 export default function Header() {
-    const [activeItem, setActiveItem] = useState('');
+    const currentPath = useLocation();
+    const finalCurrentPath = currentPath.pathname.replace("/", "");
 
+    const [activeItem, setActiveItem] = useState(finalCurrentPath);
+    const history = useHistory();
+    
     const handleItemClick = (e, { name }) =>{
         setActiveItem(name);
+        history.push(name)
     }
 
     return (
         <div className="header-menu">
             <Menu secondary>
-                <Menu.Item name="inicio" active={activeItem === "inicio"} onClick={handleItemClick} />
+                <Menu.Item name="inicio" active={activeItem === "inicio" || activeItem === ""} onClick={handleItemClick} />
                 <Menu.Item name="series" active={activeItem === "series"} onClick={handleItemClick} />
                 <Menu.Item name="comics" active={activeItem === "comics"} onClick={handleItemClick} />
             </Menu>
