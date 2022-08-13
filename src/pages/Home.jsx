b12cd1c82684f7f47a5bad84d2dff1938237cee3
Joel Trinidad <jth_2929@hotmail.com>
2022-08-13 17:36:30 -0500

    Fix: Header moved from home.jsx to app.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,7 @@
-import Header from "../components/Header/Header";
-
 export default function Home() {
   return (
     <div>
       <p>Estamos dentro de Home</p>
-      <Header />
     </div>
   );
 }
