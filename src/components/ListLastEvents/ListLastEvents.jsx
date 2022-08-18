2d2641f26214d6a1bf6d403b75f5170f2694f6c1
Joel Trinidad <jth_2929@hotmail.com>
2022-08-18 17:47:05 -0500

    Feat: events fetch

--- src/components/ListLastEvents/ListLastEvents.jsx
+++ src/components/ListLastEvents/ListLastEvents.jsx
@@ -1,8 +1,11 @@
 import { Card, Header } from "semantic-ui-react";
+import useFetch from "../../hooks/useFetch";
 import Container from "../Container";
 import "./ListLastEvents.scss";
 
 export default function ListLastEvents() {
+  const lastEventsFectch = useFetch(`${import.meta.env.VITE_APP_URL_BASE}/events?ts=1&apikey=${import.meta.env.VITE_APP_API_KEY_MARVEL}&hash=${import.meta.env.VITE_APP_HASH_ID}&orderBy=startDate&limit=5`);
+
   return (
     <div className="container-list-last-events">
       <Header size="large">Últimos eventos</Header>
