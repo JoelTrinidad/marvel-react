3a3bd44805ea8371ae94d15f790b5c78d3280c97
Joel <joel@mbp-de-joel.lan>
2022-08-22 12:17:12 -0500

    Feat: Last events displayed

--- src/components/ListLastEvents/ListLastEvents.jsx
+++ src/components/ListLastEvents/ListLastEvents.jsx
@@ -1,10 +1,11 @@
 import { Card, Header } from "semantic-ui-react";
 import useFetch from "../../hooks/useFetch";
 import Container from "../Container";
+import LastEvents from "../LastEvents";
 import "./ListLastEvents.scss";
 
 export default function ListLastEvents() {
-  const lastEventsFectch = useFetch(`${import.meta.env.VITE_APP_URL_BASE}/events?ts=1&apikey=${import.meta.env.VITE_APP_API_KEY_MARVEL}&hash=${import.meta.env.VITE_APP_HASH_ID}&orderBy=startDate&limit=5`);
+  const lastEventsFetch = useFetch(`${import.meta.env.VITE_APP_URL_BASE}/events?ts=1&apikey=${import.meta.env.VITE_APP_API_KEY_MARVEL}&hash=${import.meta.env.VITE_APP_HASH_ID}&orderBy=startDate&limit=5`);
 
   return (
     <div className="container-list-last-events">
@@ -12,12 +13,7 @@ export default function ListLastEvents() {
 
       <Container bg="Light">
         <Card.Group itemsPerRow={5}>
-          <p>eventos...</p>
-          <p>eventos...</p>
-          <p>eventos...</p>
-          <p>eventos...</p>
-          <p>eventos...</p>
-          <p>eventos...</p>
+          <LastEvents lastEventsFetch={lastEventsFetch} />
         </Card.Group>
       </Container>
     </div>
