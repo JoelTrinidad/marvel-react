e947d1f4b41200d0239ffc33bf60804d84c46cf8
Joel Trinidad <jth_2929@hotmail.com>
2022-08-22 14:24:31 -0500

    Feat: Comics page content added

--- src/pages/Comics.jsx
+++ src/pages/Comics.jsx
@@ -1,7 +1,28 @@
+import { useState } from "react";
+import { Grid, Header } from "semantic-ui-react";
+import Container from "../components/Container";
+import useFetch from "../hooks/useFetch";
+
 export default function Comics() {
+  const [renderComics, setRenderComics] = useState(10);
+
+  const listComics = useFetch(
+    `${import.meta.env.VITE_APP_URL_BASE}/comics?ts=1&apikey=${
+      import.meta.env.VITE_APP_API_KEY_MARVEL
+    }&hash=${import.meta.env.VITE_APP_HASH_ID}&orderBy=focDate&limit=${renderComics}`
+  );
+
   return (
-    <div>
-      <p>Estamos dentro de comics</p>
+    <div className="comics-page">
+      <div id="slide-comics-image"></div>
+      <Grid>
+        <Grid.Column>
+          <Container bg="Light">
+            <Header as="h2">Los mejores Cómics</Header>
+            
+          </Container>
+        </Grid.Column>
+      </Grid>
     </div>
   );
 }
